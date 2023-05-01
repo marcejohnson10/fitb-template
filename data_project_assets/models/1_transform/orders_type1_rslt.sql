8e71ed72cb63e107a446545dbec7722380aaddc5
marc.johnson@53.com <marc.johnson@53.com>
2023-05-01 22:52:16 +0000

    updates

--- data_project_assets/models/1_transform/orders_type1_rslt.sql
+++ data_project_assets/models/1_transform/orders_type1_rslt.sql
@@ -1,20 +1,27 @@
 {{
     config(
         materialized ='incremental',
-        unique_key ='I_ORDER_ID',
+        unique_key ='INGESTION_UNIQUE_KEY',
         on_schema_change='sync_all_columns',
         change_data_capture_type = '1',
         cdc_type_1_update_columns_strategy = 'include',
-        cdc_type_1_update_columns = [],
+        cdc_type_1_update_columns = [T_ORDER_STATUS,T_ORDER_SUMMARY],
         match_conditions = []
     )
 }}
 
-select {{ dbt_utils.star ( from=ref('orders_rslt') ) }},
+  
+select src_1.INGESTION_UNIQUE_KEY,
+tgt_1.INGESTION_TYPE,
+	tgt_1.INGESTION_METADATA ,
+	tgt_1.INGESTION_TIMESTAMP ,
+	tgt_1.I_ORDER_ID,
+	tgt_1.T_ORDER_STATUS ,
+	src_1.T_ORDER_SUMMARY,
        {{ get_cdc_metadata_columns('1') }}
-from {{ ref('orders_rslt') }} 
-where INGESTION_UNIQUE_KEY IN (
+from {{ ref('orders_100_rslt') }} src_1 left out join {{ this }} tgt_1 on src_1.INGESTION_UNIQUE_KEY = tgt_1.INGESTION_UNIQUE_KEY
+where src_1.INGESTION_UNIQUE_KEY IN (
         SELECT INGESTION_UNIQUE_KEY
-        FROM {{ create_stream (ref('orders_rslt')) }}
-        where METADATA$ACTION = 'INSERT')
-
+        FROM {{ create_stream (ref('orders_100_rslt')) }}
+        where METADATA$ACTION = 'INSERT')   
+        
