542b43ad19b9a262698da2e0be8f77bc254442d7
marc.johnson@53.com <marc.johnson@53.com>
2023-05-05 19:58:54 +0000

    DB Plyr

---
+++ data_project_assets/models/1_transform/get_col_names.sql
@@ -0,0 +1,15 @@
+
+{% set mylist = ['name_usage_typ','Z_CURRENT_FLAG'] %}
+{% set cols = dbtplyr.get_column_names( ref('PDP_ORG_NM_DIM') ) %}
+{% set cols_n = dbtplyr.starts_with('n', cols) %}
+{% set cols_ind = dbtplyr.starts_with('z', cols) %}
+{% set cols_one_of = dbtplyr.one_of(['name_usage_type','Z_CURRENT_FLAG'], ref('PDP_ORG_NM_DIM')) %}
+{% set cols_one_of_2 = dbtplyr.one_of(mylist, ref('PDP_ORG_NM_DIM')) %}
+
+
+{{mylist}}
+{{cols_one_of}}
+{{cols_one_of_2}}
+  {{ dbtplyr.across(cols_n, "sum({{var}}) as {{var}}_tot") }}
+  {{ dbtplyr.across(cols_ind, "mean({{var}}) as {{var}}_avg") }}
+
