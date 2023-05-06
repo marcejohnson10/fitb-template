bf445f5dad782396aac470570be6db0454f079bf
marc.johnson@53.com <marc.johnson@53.com>
2023-05-06 11:11:24 +0000

    w

--- data_project_assets/models/1_transform/get_col_names.sql
+++ data_project_assets/models/1_transform/get_col_names.sql
@@ -6,6 +6,27 @@
 {% set cols_one_of = dbtplyr.one_of(['name_usage_type','Z_CURRENT_FLAG'], ref('PDP_ORG_NM_DIM')) %}
 {% set cols_one_of_2 = dbtplyr.one_of(mylist, ref('PDP_ORG_NM_DIM')) %}
 
+{# {% if execute %} #}
+{%- set dest_col = dbt_utils.get_filtered_columns_in_relation(ref('PDP_ORG_NM_DIM')) -%}
+{%- set metadata_col_lst = ['X_LOAD_TIMESTAMP', 'X_LOAD_TIMESTAMP','X_LAST_UPDATE_TIMESTAMP'] -%}
+    {%- for metadata_col in metadata_col_lst-%}
+        {%- set my_meta_list -%}
+            XX_{{metadata_col}}
+            {%- do dest_col.append(metadata_col) -%}
+        {%- endset -%}
+        {%- set my_meta_list_2 = dest_col.append(metadata_col) -%}
+    {%- endfor-%}
+{# {%- endif-%} #}
+{#
+{%- set tgt_col -%}
+        DBT_INTERNAL_DEST.{{key}}
+{%- endset %}
+{%- do tgt_col_lst.append(tgt_col) -%} 
+#}
+
+{{dest_col}}
+{{my_meta_list_2}}
+{{my_meta_list}}
 
 {{mylist}}
 {{cols_one_of}}
