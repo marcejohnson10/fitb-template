7fc142d83fb3f8eb0c27d4cc6e59906b61198b02
marc.johnson@53.com <marc.johnson@53.com>
2023-05-09 04:26:25 +0000

    d

---
+++ data_project_assets/models/2_data_product/PR_ORG_LEGALFULLNM_DP.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized ='table'
+    )
+}}
+
+SELECT CONT_ID,
+CASE WHEN (ORGANIZATION_NAME IS NOT NULL ) THEN '1' ELSE '0' END AS Legal_Full_Nm_Populated,
+len(ORGANIZATION_NAME) AS Legal_Full_Nm_Len,
+POSITION(' ' IN ORGANIZATION_NAME) AS Legal_Full_Nm_Space_Position
+FROM {{ ref('PDP_ORG_NM_DIM') }}
+WHERE NAME_USAGE_TYPE=1000004 and Z_CURRENT_FLAG='Y'
+order by cont_id
