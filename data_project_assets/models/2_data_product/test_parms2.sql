6ca9120094bf43e2cda80c07dcd93a14dbdba55c
marc.johnson@53.com <marc.johnson@53.com>
2023-05-12 23:39:50 +0000

    s

--- data_project_assets/models/2_data_product/test_parms2.sql
+++ data_project_assets/models/2_data_product/test_parms2.sql
@@ -4,6 +4,7 @@
 {%- set existing_relation = load_cached_relation(this) -%}
 
 select distinct
+'{{env_var('DBT_CLOUD_JOB_ID','x')}}' as env_var_DBT_CLOUD_JOB_ID,
 '{{env_var('DBT_ENVIRONMENT')}}' as env_var_dbt_environments,
 '{{model.package_name}}' as package_name,
 '{{target_relation}}' as target_rel,
