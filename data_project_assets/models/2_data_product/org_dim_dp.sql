378efbcf41a1c1b54f002b4747361c23bbc5e781
marc.johnson@53.com <marc.johnson@53.com>
2023-05-19 20:32:26 +0000

    x8

--- data_project_assets/models/2_data_product/org_dim_dp.sql
+++ data_project_assets/models/2_data_product/org_dim_dp.sql
@@ -1,4 +1,4 @@
 
-select org_type_code, current_org_flag, count(cont_id)*7 as count_orgs
+select org_type_code, current_org_flag, count(cont_id)*8 as count_orgs
 from {{ ref('org_dim_rslt') }}
 group by 1, 2
