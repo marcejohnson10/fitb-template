6086819675a85eba9d3fff9bfe23370f1a809b87
marc.johnson@53.com <marc.johnson@53.com>
2023-06-12 16:45:33 +0000

    added brooklyn artifacts package

---
+++ data_project_assets/models/1_transform/org_dim_rslt_incremental.sql
@@ -0,0 +1,10 @@
+{{
+    config(
+        materialized ='incremental',
+        unique_key ='cont_id',
+        on_schema_change='sync_all_columns',
+        incremental_strategy='merge'
+    )
+}}
+
+select * from {{ ref('org_dim_rslt') }}
