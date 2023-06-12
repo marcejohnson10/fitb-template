96dada42a362b61ab08725c253749f3ed96d7f92
marc.johnson@53.com <marc.johnson@53.com>
2023-06-12 17:00:07 +0000

    s

--- data_project_assets/models/1_transform/org_dim_rslt_incremental.sql
+++ data_project_assets/models/1_transform/org_dim_rslt_incremental.sql
@@ -1,10 +1,10 @@
 {{
     config(
-        materialized ='incremental',
-        unique_key ='cont_id',
-        on_schema_change='sync_all_columns',
-        incremental_strategy='merge'
+        materialized="incremental",
+        unique_key="cont_id",
+        on_schema_change="sync_all_columns",
+        incremental_strategy="merge",
     )
 }}
 
-select * from {{ ref('org_dim_rslt') }}
+select * from {{ ref("org_dim_rslt") }}
